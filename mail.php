9fbfbe4b13fee81839e67c5f9ba9436a859b046d
Дмитрий <rudi.scorp@yandex.ru>
2018-01-08 12:56:07 +0300

    First commit

---
+++ mail.php
@@ -0,0 +1,14 @@
+<?php
+
+$recepient = "rudi.scorp@yandex.ru";
+$siteName = "Ajax-форма";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$email = trim($_POST["email"]);
+$message = "Имя: $name \nТелефон: $phone \nE-mail: $email"";
+
+$pagetitle = "Заявка с сайта \"$siteName\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+
+?>
